e90b32f6f5fe44821e2a39ee6ded35e207647818
Kieran David Evans <keyz182@gmail.com>
2018-10-07 16:55:48 +0100

    Package for npm

--- scripts/after_prepare.js
+++ scripts/after_prepare.js
@@ -8,7 +8,7 @@ module.exports = function(context) {
     if(parseInt(context.opts.cordova.version.split('.')[0], 10) >= 7){
         var config = fs.readFileSync("package.json").toString();
         var json = JSON.parse(config);
-        var APP_ID = json["cordova"]["plugins"]["cordova-plugin-facebook4"]["APP_ID"];
+        var APP_ID = json["cordova"]["plugins"]["cordova-plugin-facebook4-asb"]["APP_ID"];
     }
     else // Cordova < 7, use config.xml
     {
@@ -30,8 +30,8 @@ module.exports = function(context) {
     }
 
     var files = [
-        "platforms/browser/www/plugins/cordova-plugin-facebook4/www/facebook-browser.js",
-        "platforms/browser/platform_www/plugins/cordova-plugin-facebook4/www/facebook-browser.js",
+        "platforms/browser/www/plugins/cordova-plugin-facebook4-asb/www/facebook-browser.js",
+        "platforms/browser/platform_www/plugins/cordova-plugin-facebook4-asb/www/facebook-browser.js",
         "platforms/browser/www/cordova.js",
         "platforms/browser/platform_www/cordova.js"
     ];
